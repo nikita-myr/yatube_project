8b251b8b02402668cb8a6ae0f9f99006f7621028
Nikita Muravskiy <nik.myr.2000@gmail.com>
2022-04-19 20:31:35 +0300

    Added URLS and view functions

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def index(request):
+    return HttpResponse('THE HOME PAGE WILL BE HERE SOON')
+
+def group_posts(request, slug):
+    return HttpResponse('AND HERE WILL BE USERS POSTS')
