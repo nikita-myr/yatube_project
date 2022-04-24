d45d7839a6206ac8404ad0c812fb71b377dfcf6d
Nikita Muravskiy <nik.myr.2000@gmail.com>
2022-04-24 19:04:42 +0300

    pre test commit... added posts, group, header, footer, logo, css.. and other

--- yatube/posts/urls.py
+++ yatube/posts/urls.py
@@ -2,7 +2,10 @@ from django.urls import path
 from . import views
 
 
+app_name = 'posts'
+
+
 urlpatterns = [
-    path('', views.index), #Main page
-    path('group/<slug:slug>/',views.group_posts), # Group page
+    path('', views.index, name='index'),  # Main page
+    path('group/<slug:slug>/', views.group_posts, name='group_list'),  # Group page
 ]
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,7 +1,23 @@
-from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, render
+from .models import Group, Post
+
 
 def index(request):
-    return HttpResponse('THE HOME PAGE WILL BE HERE SOON')
+    '''Main page response'''
+    template = 'posts/index.html'
+    posts = Post.objects.order_by('-pub_date')[:10]
+    context = {
+                'posts': posts
+             }
+    return render(request, template, context)
+
 
 def group_posts(request, slug):
-    return HttpResponse('AND HERE WILL BE USERS POSTS')
+    template = 'posts/group_list.html'
+    group = get_object_or_404(Group, slug=slug)
+    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
+    context = {
+                'group': group,
+                'posts': posts
+                }
+    return render(request, template, context)
